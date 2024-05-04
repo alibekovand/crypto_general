ec8887469e9f7b362f13c0674900997ea7f94684
Andrey Alibekov <alibekovand@gmail.com>
2024-05-04 15:00:41 +0700

    update

--- rsa.py
+++ rsa.py
@@ -1,125 +1,122 @@
 import random
 
-'''
-Euclid's algorithm for determining the greatest common divisor
-Use iteration to make it faster for larger integers
-'''
+
+# алгоритм Евклида нахождения обратного числа a по модулю m
+def mod_inverse(a, m):
+    """
+    :param a - это число, для которого мы хотим найти обратное по модулю:
+    :param m - модуль, по которому мы хотим найти обратное значение числа:
+    :return число обратное a по модулю m:
+    """
+    m0, x0, x1 = m, 0, 1
+    while a > 1:
+        q = a // m
+        m, a = a % m, m
+        x0, x1 = x1 - q * x0, x0
+    return x1 + m0 if x1 < 0 else x1
 
 
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
-
-
-'''
-Euclid's extended algorithm for finding the multiplicative inverse of two numbers
-'''
-
-
-def multiplicative_inverse(e, phi):
-    d = 0
-    x1 = 0
-    x2 = 1
-    y1 = 1
-    temp_phi = phi
-
-    while e > 0:
-        temp1 = temp_phi//e
-        temp2 = temp_phi - temp1 * e
-        temp_phi = e
-        e = temp2
-
-        x = x2 - temp1 * x1
-        y = d - temp1 * y1
-
-        x2 = x1
-        x1 = x
-        d = y1
-        y1 = y
-
-    if temp_phi == 1:
-        return d + phi
-
-
-'''
-Tests to see if a number is prime.
-'''
-
-
-def is_prime(num):
-    if num == 2:
-        return True
-    if num < 2 or num % 2 == 0:
+# Функция предназначена для проверки, является ли заданное число 'n' простым числом.
+def is_prime(n, k=5):
+    """
+    :param n - Целое число, для которого проверяется простота:
+    :param k - Количество итераций, используемых в тесте простоты. Данным параметром мф контролируем насколько точной будет проверка на простоту
+    :return True - число простое
+    :return False - число не простое:
+    """
+    if n <= 1:
         return False
-    for n in range(3, int(num**0.5)+2, 2):
-        if num % n == 0:
+    if n <= 3:
+        return True
+    for _ in range(k):
+        a = random.randint(2, n - 1)
+        if pow(a, n - 1, n) != 1:
             return False
     return True
 
 
-def generate_key_pair(p, q):
-    if not (is_prime(p) and is_prime(q)):
-        raise ValueError('Both numbers must be prime.')
-    elif p == q:
-        raise ValueError('p and q cannot be equal')
-    # n = pq
+#Функция для генерации простого числа
+def generate_prime_number(bits):
+    """
+    :param bits: Количество битов, которое должно содержать сгенерированное простое число
+    :return num: сгенерированное простое число
+    """
+    while True:
+        num = random.getrandbits(bits)
+        if is_prime(num):
+            return num
+
+
+
+
+# Функция для генерации пары ключей
+def generate_keypair(bitlength):
+    """
+    :param bitlength: Длина ключа в битах
+    :return (e, n), (d, n):
+        e: открытая экспонента.
+        n: произведение двух простых чисел, которое становится модулем для обоих ключей.
+        d: закрытый ключ. Число обратное 'e' по модулю phi.
+
+    """
+    half_bitlength = bitlength // 2
+    p = generate_prime_number(half_bitlength)
+    q = generate_prime_number(half_bitlength)
     n = p * q
-
-    # Phi is the totient of n
-    phi = (p-1) * (q-1)
-
-    # Choose an integer e such that e and phi(n) are coprime
-    e = random.randrange(1, phi)
-
-    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
-    g = gcd(e, phi)
-    while g != 1:
-        e = random.randrange(1, phi)
-        g = gcd(e, phi)
-
-    # Use Extended Euclid's Algorithm to generate the private key
-    d = multiplicative_inverse(e, phi)
-
-    # Return public and private key_pair
-    # Public key is (e, n) and private key is (d, n)
+    phi = (p - 1) * (q - 1)
+    e = 65537  # Обычно в качестве открытого экспонента выбирают число 65537
+    d = mod_inverse(e, phi)
     return ((e, n), (d, n))
 
 
-def encrypt(pk, plaintext):
-    # Unpack the key into it's components
-    key, n = pk
-    # Convert each letter in the plaintext to numbers based on the character using a^b mod m
-    cipher = [pow(ord(char), key, n) for char in plaintext]
-    # Return the array of bytes
+
+# Функция для шифрования сообщения
+def encrypt(public_key, plaintext):
+    """
+    :param public_key:  Открытый ключ RSA, представленный в виде кортежа (e, n), где e - открытая экспонента, а n - модуль
+    :param plaintext: Сообщение
+    :return cipher: список зашифрованных символов
+    """
+
+    """
+    pow(x, y, z) - функция возводит число 'x' в степень 'y' и берет модуль 'z' 
+    ord(c) - представляет символ в числовом формате
+    """
+    e, n = public_key
+    cipher = [pow(ord(char), e, n) for char in plaintext]
     return cipher
 
 
-def decrypt(pk, ciphertext):
-    # Unpack the key into its components
-    key, n = pk
-    # Generate the plaintext based on the ciphertext and key using a^b mod m
-    aux = [str(pow(char, key, n)) for char in ciphertext]
-    # Return the array of bytes as a string
-    plain = [chr(int(char2)) for char2 in aux]
+# функция для расшифровки зашифрованного сообщения
+def decrypt(private_key, cipher):
+    """
+
+    :param private_key: Закрытый ключ RSA, представленный в виде кортежа (d, n), где d - закрытая экспонента, а n - модуль.
+    :param cipher: Зашифрованное сообщение, представленное в виде списка чисел
+    :return: Расшифрованное сообщение
+    """
+    d, n = private_key
+    plain = [chr(pow(char, d, n)) for char in cipher]
     return ''.join(plain)
 
-
+# Пример использования:
 if __name__ == '__main__':
 
+    # Генерация ключей
+    public_key, private_key = generate_keypair(128)
 
-    p = int(input(" - Enter a prime number (17, 19, 23, etc): "))
-    q = int(input(" - Enter another prime number (Not one you entered above): "))
+    # Вывод ключей
+    print("public key:", public_key)
+    print("private key:", private_key)
 
-    print(" - Generating your public / private key-pairs now . . .")
+    # Шифрование
+    plaintext = "Hello, World!"
+    cipher = encrypt(public_key, plaintext)
+    print("Encrypted:", cipher)
 
-    public, private = generate_key_pair(p, q)
+    # Расшифрование
+    decrypted = decrypt(private_key, cipher)
+    print("Decrypted:", decrypted)
 
-    print(" - Your public key is ", public, " and your private key is ", private)
 
-    message = input(" - Enter a message to encrypt with your public key: ")
-    encrypted_msg = encrypt(public, message)
 
-    print(" - Your encrypted message is: ", ''.join(map(lambda x: str(x), encrypted_msg)))
-    print(" - Decrypting message with private key ", private, " . . .")
-    print(" - Your message is: ", decrypt(private, encrypted_msg))
